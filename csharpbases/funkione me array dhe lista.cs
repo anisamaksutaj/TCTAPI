d78e552dd31bc9a3c399359249d08b7f93db3248
AnisaMaksutaj <anisamaksutaj@hotmail.com>
2023-02-14 19:48:32 +0100

    Shtuam nje Klase me dy fuksione me e madhe dhe shumen

---
+++ csharpbases/funkione me array dhe lista.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpbases
+{
+    internal class funkione_me_array_dhe_lista
+    {
+        public int GjejMeTeMadhin(int[] array)
+        {
+
+            int max = 0;
+            for( int i=0; i<array.Length;i++ )
+            {
+                if (array[i]> max ) max = array[i];
+            }
+
+            return max;
+            
+        }
+        public int GjejShumenEArray(int[] array)
+        {
+            int sum = 0;
+            for(int=0; i<array.Length; int++)
+            {
+                sum = sum + array[i];
+            }
+            return sum;
+        }
+
+
+
+
+
+
+    }
+}
